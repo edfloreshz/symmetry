43aae6b4b41526c3069b193dd5ddac3110df83ed
AIDXNZ <asmaidan@outlook.com>
2023-05-12 13:57:42 -0500

    " Minimal +fn update()"

--- core/src/sync/providers/crdt.rs
+++ core/src/sync/providers/crdt.rs
@@ -14,8 +14,7 @@ struct CRDTSync {
     pub config: CrdtConfig,
     pub peers: Vec<SocketAddr>,
     pub ops: crdts::List<String, String>,
-    pub curr_setting: crdts::LWWReg<String, i32>
-
+    pub curr_setting: crdts::LWWReg<String, i32>,
 }
 
 impl Synchronization for CRDTSync {
@@ -28,13 +27,36 @@ impl Synchronization for CRDTSync {
 
     fn handle(&self, message: Self::Message) -> Result<()> {
         match message {
-           Message::Update => todo!(),
+            Message::Update => todo!(),
         }
     }
 }
 
 impl CRDTSync {
-    pub fn new(config: CrdtConfig) -> Self {
-        Self { config, peers: vec![], ops: List::new(), curr_setting: crdts::LWWReg { val: "S".to_string(), marker: 0 }}
+    pub fn new() -> Self {
+        Self {
+            config: CrdtConfig::default(),
+            peers: vec![],
+            ops: List::new(),
+            curr_setting: crdts::LWWReg {
+                val: "".to_string(),
+                marker: 0,
+            },
+        }
+    }
+    pub fn update(&mut self, val: String) { 
+        let cur_marker = self.curr_setting.marker + 1;
+        self.ops.append(val.to_string(), "A".to_string());
+        self.curr_setting.update(val, cur_marker);
+        
+    }
+
+    pub fn listen(&mut self) {
+        todo!()
+    }
+
+    // 
+    pub fn fetch(&mut self) {
+        todo!()
     }
 }
