cb17f25d847dad3acb186681f31eba012fc3403d
AIDXNZ <asmaidan@outlook.com>
2023-05-12 13:58:20 -0500

    "Added dalek"

--- core/src/sync/providers/config/crdt.rs
+++ core/src/sync/providers/config/crdt.rs
@@ -1,6 +1,22 @@
+use ed25519_dalek::{Keypair, PublicKey, SecretKey};
+use rand_core::OsRng;
+
 #[derive(Debug, Default)]
 pub struct CrdtConfig {
-    pub key: Vec<u8>,
-    pub id: String,
+    pub pk: [u8; 32],
+    secret: [u8; 32],
     pub enabled: bool,
+}
+
+
+impl CrdtConfig {
+    pub fn new() -> Self {
+        let mut csprng = OsRng{};
+        let keypair: Keypair = Keypair::generate(&mut csprng);
+        Self { pk: keypair.public.to_bytes(),secret: keypair.secret.to_bytes(), enabled: true }
+    }
+
+    pub fn default() -> Self {
+        CrdtConfig::new()
+    }
 }
